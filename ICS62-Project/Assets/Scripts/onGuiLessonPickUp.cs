4a372d23c80b4abcf4c295e8a21f4c1eb9014b3b
jrabaino <jrabaino@uci.edu>
2015-05-12 12:39:30 -0700

    added new gui test script

---
+++ ICS62-Project/Assets/Scripts/onGuiLessonPickUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onGuiLessonPickUp : MonoBehaviour {
+	public bool show = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnGUI(){
+		
+		if (show) {
+			GUI.Label (new Rect (10, 10, 200, 50), "gui test");
+		}
+		
+	}
+	// Update is called once per frame
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			showInventory ();
+		}
+	}
+	
+	void showInventory(){
+		if (show) {
+			show = false;
+		} else {
+			show = true;
+		}
+	}
+	
+}
+
