378f8af28beae990a2edd240b876dff80f7180ca
jrabaino <jrabaino@uci.edu>
2015-05-12 22:29:20 -0700

    added movement and and pick up of key
    still needs the pop-up window when the lesson is picked up.

---
+++ ICS62-Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public float rotation;
+	public static bool AllowMove = true;
+	CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (AllowMove) {
+			Vector3 forward = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.down) * speed;
+			transform.Rotate (new Vector3 (0, 0, Input.GetAxis ("Horizontal") * rotation * Time.deltaTime));
+			cc.Move (forward * Time.deltaTime);
+			cc.SimpleMove (Physics.gravity);
+		}
+
+	}
+
+	public static void MovementBool(){
+		if (AllowMove) {
+			AllowMove = false;
+		} else {
+			AllowMove = true;
+		}
+	}
+
+
+
+}
---
+++ ICS62-Project/Assets/Scripts/eventPlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eventPlayerController : MonoBehaviour {
+
+	public int pickCount;
+	void Start () {
+		pickCount = 0;
+	}
+	void OnGUI(){
+		GUI.Label (new Rect (10, 10, 200, 50), "Lessons collected: " + pickCount.ToString()+" /10");
+	}
+
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.CompareTag("Pickup")) {
+			other.gameObject.SetActive (false);
+			pickCount += 1;
+			PlayerMovement.MovementBool();
+
+		}
+	}
+}
