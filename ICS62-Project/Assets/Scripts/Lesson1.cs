9ef9e41803bc01287fbbe36254a0d0bde58487fe
changzhang <albertc7@uci.edu>
2015-05-27 09:09:54 -0700

    onGuiUpdate

--- ICS62-Project/Assets/Scripts/Lesson1.cs
+++ ICS62-Project/Assets/Scripts/Lesson1.cs
@@ -10,6 +10,7 @@ public class Lesson1 : MonoBehaviour {
 	public Texture dotted_half;
 	public Texture whole;
 	private int health;
+	private bool tracker;
 	private string answer;
 	private string question;
 	private List<string> questions = new List<string>();
@@ -17,6 +18,7 @@ public class Lesson1 : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
+		tracker = true;
 		health = 5;
 		questions.Add ("eighth");
 		questions.Add ("quarter");
@@ -24,50 +26,85 @@ public class Lesson1 : MonoBehaviour {
 		questions.Add ("dotted_half");
 		questions.Add ("whole");
 
-		while (health > 0) 
-		{
-			Debug.Log (1);
-			while (questions.Count>0) 
-			{
-				Debug.Log (2);
-				question = questions [Random.Range (0, questions.Count)];
-				OnGUI ();
-				Debug.Log (answer);
-				Debug.Log (question);
-				break;
-			}
-			if (answer == question)
-			{
-				health = health - 1;
-				questions.Remove(question);
-			}
-			break;
-		}
+
 	}
 
+	void Update()
+	{
+		if (tracker == true) 
+		{
+			question = questions [Random.Range (0, questions.Count)];
+			tracker = false;
+		}
+
+		while (questions.Count>0) 
+		{
+			Debug.Log (2);
+			//question = questions [Random.Range (0, questions.Count)];
+			OnGUI ();
+			Debug.Log (answer);
+			break;
+		}
+
+		if (answer == question)
+		{
+			questions.Remove(question);
+			tracker = true;
+			health = health - 1;
+		}
+
+	}
+	
 	void OnGUI()
 	{	
-		if (question == "eighth") {
+
+		if (question == "eighth") 
+		{
 			GUI.Label (new Rect (Screen.width / 2-100, Screen.height / 4, 500, 100), "Which note is the eighth note?");
-		} else if (question == "quarter") {
+		} 
+
+		else if (question == "quarter") 
+		{
 			GUI.Label (new Rect (Screen.width / 2-100, Screen.height / 4, 500, 100), "Which note is the quarter note?");
-		} else if (question == "half") {
+		} 
+
+		else if (question == "half") 
+		{
 			GUI.Label (new Rect (Screen.width / 2-100, Screen.height / 4, 500, 100), "Which note is the half note?");
-		} else if (question == "dotted_half") {
+		} 
+
+		else if (question == "dotted_half") 
+		{
 			GUI.Label (new Rect (Screen.width / 2-100, Screen.height / 4, 500, 100), "Which note is the dotted half note?");
-		} else if (question == "whole") {
+		} 
+
+		else if (question == "whole") 
+		{
 			GUI.Label (new Rect (Screen.width / 2-100, Screen.height / 4, 500, 100), "Which note is the whole note?");
 		}
 
-		if (GUI.Button (new Rect (Screen.width / 10, Screen.height / 2, 100, 100), eighth)) {
+		if (GUI.Button (new Rect (Screen.width / 10, Screen.height / 2, 100, 100), eighth)) 
+		{
 			answer = "eighth";
-		} else if (GUI.Button (new Rect (Screen.width / 10 * 3, Screen.height / 2, 100, 100), quarter)) {
+		} 
+
+		else if (GUI.Button (new Rect (Screen.width / 10 * 3, Screen.height / 2, 100, 100), quarter)) 
+		{
 			answer = "quarter";
-		} else if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2, 100, 100), half)) {
+		} 
+
+		else if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2, 100, 100), half)) 
+		{
 			answer = "half";
-		} else if (GUI.Button (new Rect (Screen.width / 10 * 7, Screen.height / 2, 100, 100), dotted_half)) {
+		} 
+
+		else if (GUI.Button (new Rect (Screen.width / 10 * 7, Screen.height / 2, 100, 100), dotted_half)) 
+		{
 			answer = "dotted_half";
-		} else if (GUI.Button (new Rect (Screen.width / 10 * 9, Screen.height / 2, 100, 100), whole)) {
+		} 
+
+		else if (GUI.Button (new Rect (Screen.width / 10 * 9, Screen.height / 2, 100, 100), whole)) 
+		{
 			answer = "whole";
 		} 
 
--- ICS62-Project/Assets/Scripts/eventPlayerController.cs
+++ ICS62-Project/Assets/Scripts/eventPlayerController.cs
@@ -5,16 +5,34 @@ public class eventPlayerController : MonoBehaviour {
 
 	public int pickCount;
 	public Texture imgTexture;
+	public Texture eighth;
+	public Texture quarter;
+	public Texture half;
+	public Texture dotted_half;
+	public Texture whole;
 	public GUIText lessonText;
 
 	void Start () {
 		pickCount = 0;
 	}
+	
 	void OnGUI(){
 		GUI.Label (new Rect (10, 10, 200, 50), "Lessons collected: " + pickCount.ToString()+" /10");
 		if (onGuiLessonPickUp.show) {
 			onGuiLessonPickUp.lesson1(imgTexture);
 
+		}
+		if (onGuiLessonPickUp.plan) {
+
+			onGuiLessonPickUp.questions.Add ("eighth");
+			onGuiLessonPickUp.questions.Add ("quarter");
+			onGuiLessonPickUp.questions.Add ("half");
+			onGuiLessonPickUp.questions.Add ("dotted_half");
+			onGuiLessonPickUp.questions.Add ("whole");
+			while (onGuiLessonPickUp.health > 0){
+				onGuiLessonPickUp.Lesson1Plan (eighth, quarter, half, dotted_half, whole);
+				break;
+			}
 		}
 	}
 
